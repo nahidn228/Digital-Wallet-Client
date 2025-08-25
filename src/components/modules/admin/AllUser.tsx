b5242f411cabb2d76ff88a62bb45013b6b57e273
nahidn228 <nahidn228@gmail.com>
2025-08-26 00:42:10 +0600

    implement pagination with active and disable btn

--- src/components/modules/admin/AllUser.tsx
+++ src/components/modules/admin/AllUser.tsx
@@ -17,7 +17,7 @@ import {
   SelectValue,
 } from "@/components/ui/select";
 import { useGetAllUserQuery } from "@/redux/features/user/user.api";
-import { Trash2 } from "lucide-react";
+import { ChevronFirst, ChevronLast, Trash2 } from "lucide-react";
 
 interface IUser {
   _id: string;
@@ -38,13 +38,14 @@ const AllUser = () => {
 
   const { data, refetch } = useGetAllUserQuery({
     page,
-    limit: 10,
+    limit: 5,
     email: searchEmail,
     role: filterRole === "all" ? null : filterRole,
   });
 
   const users = data?.data?.users;
-  const pagination = data?.data?.pagination;
+  const total = data?.data?.total || 0;
+  const totalPages = Math.ceil(total / 5);
 
   // Refetch on filters change
   useEffect(() => {
@@ -57,9 +58,8 @@ const AllUser = () => {
       <div className="container mx-auto px-4">
         <div className="mb-16 text-center">
           <h2 className="mb-6 text-3xl font-bold  lg:text-4xl">
-           All D.<span className="text-primary">Wallet </span> Members
+            All D.<span className="text-primary">Wallet </span> Members
           </h2>
-          
         </div>
         {/* Filters */}
         <div className="flex gap-2 mb-4 flex-col md:flex-row">
@@ -83,7 +83,7 @@ const AllUser = () => {
         </div>
 
         {/* Table */}
-        <div className="bg-background overflow-hidden rounded-md border">
+        <div className="bg-background overflow-hidden rounded-md border min-h-80">
           <Table>
             <TableHeader>
               <TableRow className="bg-muted/50">
@@ -124,16 +124,16 @@ const AllUser = () => {
             disabled={page === 1}
             onClick={() => setPage((prev) => prev - 1)}
           >
-            Prev
+            <ChevronFirst /> Prev
           </Button>
           <span>
-            Page {pagination?.page} of {pagination?.totalPages || 1}
+            Page {page} of {totalPages || 1}
           </span>
           <Button
-            disabled={page === pagination?.totalPages}
+            disabled={page === totalPages || totalPages === 0}
             onClick={() => setPage((prev) => prev + 1)}
           >
-            Next
+            Next <ChevronLast />
           </Button>
         </div>
       </div>
