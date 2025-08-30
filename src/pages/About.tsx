5d550b6c054c8ea2436bb982a32da1b48f22ba27
nahidn228 <nahidn228@gmail.com>
2025-08-30 23:56:41 +0600

    update UI with motion

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,25 +1,31 @@
 import AboutSection from "@/components/AboutPage/AboutSection";
 import { Loader } from "@/components/ui/Loader";
 import { useEffect, useState } from "react";
-
+import { motion } from "framer-motion";
 const About = () => {
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      const timer = setTimeout(() => {
-        setLoading(false);
-      }, 3000);
-  
-      return () => clearTimeout(timer);
-    }, []);
-  
-    if (loading) {
-      return <Loader />;
-    }
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
   return (
-    <div>
+    <motion.div
+      className="w-full"
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+    >
       <AboutSection />
-    </div>
+    </motion.div>
   );
 };
 
